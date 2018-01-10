4df15c7b56d2bed666d9956746979f4368428dbe
Mitya Teryaew <mitya.teryaew@gmail.com>
2018-01-10 17:28:27 +0300

    chore(*): add validation

--- utils/validate.js
+++ utils/validate.js
@@ -1,29 +1,23 @@
 #!/usr/bin/env node
 
-/*
- * Precommit actions:
- * 1. Check file naming policy
- * 2. Optimize added icons via svgo
- */
-
 const exec = require('child_process').execSync;
 const path = require('path');
 
 const FILENAME_REGEXP = /(icon|art)_(?!-)[a-z-]+(?!-)_(xs|s|m|l|xl|xxl)_(black|color|white)\.svg$/i;
 
-function getGitCachedFiles(glob) {
-    return exec(`git diff --cached --name-only --diff-filter=ACMR ${glob}`).toString().trim();
+function getFilesInCommit() {
+    return exec('git diff-tree --no-commit-id --name-only -r `git rev-parse --verify HEAD`').toString().trim();
 }
 
-getGitCachedFiles('*.svg')
+getFilesInCommit()
     .split('\n')
-    .filter(item => item)
+    .filter(item => path.extname(item) === '.svg')
     .forEach(file => {
         if (!FILENAME_REGEXP.test(file)) {
             throw `Error! Incorrect file name for ${file}. It must follow the pattern 'icon_name_size_theme.svg'.`;
         }
 
         exec(`./node_modules/.bin/svgo --disable removeViewBox ${path.resolve(file)}`, { stdio: 'inherit' });
-        exec('git add .');
+        exec('git add . && git commit -m "chore(*): optimize svg" && git push');
     }
 );
