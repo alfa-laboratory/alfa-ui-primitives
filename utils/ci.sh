4331f2a3d7f4e778aa7c706d492980b8f1acc6cc
dmitrsavk <dmitrsavk@yandex.ru>
2020-07-14 13:46:27 +0300

    add publish ci
    add test branch
    
    fix
    
    fix
    
    debug
    
    debug
    
    debug
    
    delete log
    
    add npm i
    
    fix
    
    fix
    
    add bot emaik
    
    debug
    
    debug
    
    fix for prod

---
+++ utils/ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git fetch --prune --unshallow
+git fetch --tags
+
+git_commits_diff=`git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline | grep -P "add \d+ icons"`
+
+if [ -z "$git_commits_diff" ]
+then
+      echo "No new icon commits found"
+else
+      echo "New icon commits found, publish..."
+      npm i
+
+      git config --global user.email "ds@alfabank.ru"
+      git config --global user.name "alfa-bot"
+      git add package-lock.json
+      git commit -m 'chore(*): update package-lock.json'
+
+      npm run release-minor
+fi
