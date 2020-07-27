989070df6b5b39f72b44c78d76809500f5830906
dmitrsavk <dmitrsavk@yandex.ru>
2020-07-27 12:10:08 +0300

    chore(*): update gitignore, remove debug info, fix autopublishing

--- utils/publish.sh
+++ utils/publish.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-npm set registry https://registry.npmjs.org
-npm whoami
-
 rm -rf .publish
 mkdir .publish
 cp -r {icons,styles,package.json,README.md} .publish
-cd .publish
 npm publish
 
 echo "Publishing package to registry..."
