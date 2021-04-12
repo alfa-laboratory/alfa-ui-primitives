0d35204b0d5ff330a20fce35a4fe149f068aa3d4
dmitrsavk <dmitrsavk@yandex.ru>
2021-04-12 11:29:07 +0300

    chore(*): update ci

--- utils/ci.sh
+++ utils/ci.sh
@@ -5,20 +5,28 @@ git fetch --prune --unshallow
 git fetch --tags
 
 # Смотрим, были ли добавлены новые иконки
-git_commits_diff=`git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline | grep "feat(icons)"`
+LATEST_TAG=$(git describe --tags --abbrev=0)
+CHANGED_ICONS=$(git diff --name-only $LATEST_TAG HEAD | grep "icons/")
+DELETED_ICONS=$(git diff --name-only $LATEST_TAG HEAD --diff-filter=D | grep "icons/")
 
-# Если были добавлены новые иконки, релизим минорную версию пакета
-if [ -z "$git_commits_diff" ]
+# Если были добавлены новые иконки, релизим новую версию пакета
+if [ -z "$CHANGED_ICONS" ]
 then
-      echo "No new icon commits found"
+    echo "No new icon commits found"
 else
-      echo "New icon commits found, publish..."
-      npm i
+    echo "New icon commits found, publish..."
+    npm i
 
-      git config --global user.email "ds@alfabank.ru"
-      git config --global user.name "alfa-bot"
-      git add package-lock.json
-      git commit -m 'chore(*): update package-lock.json'
+    git config --global user.email "ds@alfabank.ru"
+    git config --global user.name "alfa-bot"
+    git add package-lock.json
+    git commit -m 'chore(*): update package-lock.json'
 
-      npm run release-minor
+    # Если какие-то иконки были удалены, релизим мажорную версию пакета
+    if [ -z "$DELETED_ICONS" ]
+    then
+        npm run release-minor
+    else
+        npm run release-major
+    fi
 fi
